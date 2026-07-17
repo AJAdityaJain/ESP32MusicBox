1841966bf0eaf930f7cfff01e4fe4928ee2d9080
Aditya Jain <adityajain121108@gmail.com>
2026-07-17 15:04:03 +0530

    load screen

--- ESP32MusicBox.ino
+++ ESP32MusicBox.ino
@@ -1,6 +1,9 @@
 #include "sd_interface.h"
 #include "freertos/semphr.h"
 #define BT_DEVICE_NAME "Bose QC35 II AJ"
+#include <U8g2lib.h>
+#include <Wire.h>
+
 
 using namespace libhelix;
 
@@ -12,6 +15,10 @@ static bool is_connected_or_connecting = false;
 static bool has_paired_addr = false;
 static esp_bd_addr_t paired_addr = {0};
 BluetoothA2DPSource a2dp_source;
+
+
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 void connection_state_changed(esp_a2d_connection_state_t state, void *ptr)
 {
@@ -34,20 +41,39 @@ void connection_state_changed(esp_a2d_connection_state_t state, void *ptr)
 
 void setup()
 {
+  delay(50);
   Serial.begin(115200);
-  delay(1000);
+  u8g2.begin();
+  u8g2.setDisplayRotation(U8G2_R2);
+  u8g2.setFont(u8g2_font_ncenB08_tr);	
+
+  u8g2.drawLine(1,1,1,10);
 
   if (init_sd())
   {
     has_paired_addr = load_addr(paired_addr);
   }
+  else{
+    u8g2.drawStr(1,20,"memory card missing");
+    u8g2.sendBuffer();
+    return;
+  }
+  u8g2.drawLine(1,1,10,1);
+
+  u8g2.sendBuffer();
+  delay(5);
 
   mp3File = SD.open("/aud.mp3");
+  u8g2.drawLine(126,52,126,62);
+
   if (!mp3File)
   {
     Serial.println("File not found");
     return;
   }
+  u8g2.sendBuffer();
+  delay(5);
+
 
   pcmMutex = xSemaphoreCreateMutex();
   helix.begin();
@@ -58,7 +84,10 @@ void setup()
   a2dp_source.set_on_audio_state_changed(audio_state_changed); // ← add this
   a2dp_source.set_data_callback_in_frames(get_data_frames);
   a2dp_source.start();
-  delay(1000);
+  u8g2.drawStr(55,45,"LIRA");
+  u8g2.sendBuffer();
+
+  delay(900);
   a2dp_source.connect_to(paired_addr);
 }
 
